c969ded58e5b0824de5a6dcfbc3de495ed413ac5
max <mdsiurds@gmail.com>
2025-05-06 21:20:01 +0200

    memset degager

--- mandatory/minishell.h
+++ mandatory/minishell.h
@@ -38,14 +38,6 @@ typedef struct s_pipe
 {
 	char ***cmd_args; // [numero de pipe]{"ls", "-l", NULL}
 	char **cmd_path; // [numero de pipe]"/bin/ls"
-	char ***infile;   // NULL si pas de redirection [pipe][infile][characteres]
-	//int					nb_infile;
-	char ***outfile; // [numero de pipe]NULL si pas de redirection
-	char ***append;
-	//int					nb_outfile;
-	int **pipe_in;    // 1 si doit lire d’un pipe // 0 si doit lire d'un infile ?
-	int **pipe_out;   // 1 si doit écrire dans un pipe																							{0, 1, 1, 0, 1, 1} pipe0 = echo >out >>out1 >>out2 >out3 >>out4 >>out5 | >out6 >out7 >>out8 >out21 >>out22 | cat  | >out9
-	int **append;     // 1 si ">>" (ajoute a la fin) // 0 si ">" (efface le fichier)  Initialiser à -1	Pour détecter les erreurs facilement	{0, 0, 1, 0, 1} pipe1
 	int pipe;        // numero du pipe	 																										{-1} pipe2
 	int					nb_pipe;																									//			{0} pipe3
 }							t_pipe;
