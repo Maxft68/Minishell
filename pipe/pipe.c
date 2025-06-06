1a6dece3e042b3059a2b3a0f836b2f15d3bd0340
max <mdsiurds@gmail.com>
2025-06-06 07:48:31 +0200

    merge main dans max 6.06 7h40

--- pipe/pipe.c
+++ pipe/pipe.c
@@ -3,6 +3,34 @@
 #include "../mandatory/minishell.h"
 
 
+<<<<<<< Updated upstream
+=======
+char	*find_path_cmd(t_all *all, char **env)
+{
+	char **path_to_search = NULL;
+	int i = 0;
+	char *path;
+
+	while (env && env[i] && ft_strncmp(env[i], "PATH=", 5) != 0)
+		i++;
+	if (!env[i]) //quand env nexiste plus
+	{
+		// ft_putstr_fd(args1, 2);
+		// ft_putstr_fd(": command not found\n", 2);   a modifier
+		// exit(127);
+	}
+	path_to_search = gc_split(all, env[i] + 5, ':');
+	if (!path_to_search) //quel cas echoue ?
+	{
+		// exit(1); a modif
+	}
+	path = search_good_path(path_to_search, all);
+	if (!path)
+		return(ft_exit("PAS DE PATH, PAS DE CHOCOLAT", all, 127), NULL);
+	return(path);
+}
+
+>>>>>>> Stashed changes
 void	exec_cmd(t_all *all)
 {
 	char **env = do_char_env(all);
@@ -19,8 +47,12 @@ void	exec_cmd(t_all *all)
 	pipe = all->pipe.pipe;
 	if (!all->pipe.cmd_args || !all->pipe.cmd_args[pipe])
 	{
+<<<<<<< Updated upstream
 		//free_array(env); dans gc malloc env export
 		printf("-----------REGIS TU MAS PAS DONNER DE CMD :O JE FAIS QUOI ?oO---------------------"); // cas possible si pas de cmd donc pas de ft_exit a faire. a enlever plus tard
+=======
+		printf("-----------REGIS TU MAS PAS DONNER DE CMD :O JE FAIS QUOI ?oO---------------------\n"); // cas possible si pas de cmd donc pas de ft_exit a faire. a enlever plus tard
+>>>>>>> Stashed changes
 		return ;
 	}
 	cmd = all->pipe.cmd_args[pipe];
@@ -48,7 +80,7 @@ void	exec_cmd(t_all *all)
 	}
 	/* path = NULL;
 	path = all->pipe.cmd_path[pipe]; */
-	
+
 	if (execve(path, cmd, env) == -1)
 		printf("-=-=-=-execve fail-=-=--\n");
 }
@@ -81,7 +113,6 @@ char	*search_good_path(char **paths, t_all *all)
 		tmp = gc_strjoin3(paths[i], "/", all->pipe.cmd_args[all->pipe.pipe][0], all);
 		if (access(tmp, X_OK) == 0)
 			return (tmp);
-		//free(tmp); // chemin non trouvee donc soit env inexistant soit cmd invalide
 		i++;
 	}
 	ft_putstr_fd(all->pipe.cmd_args[all->pipe.pipe][0], 2); // ?? 
