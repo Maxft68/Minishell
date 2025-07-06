f77bb7f51e0d7719838941cc1ba9e1c488f276c1
max <mdsiurds@gmail.com>
2025-07-06 02:41:02 +0200

    exit fini jpense

--- mandatory/minishell.c
+++ mandatory/minishell.c
@@ -71,6 +71,6 @@ int	main(int argc, char **argv, char **env)
 	// printf("d_quote= %s\n", all.lexer->d_quote ? "true" : "false");
 	// printf("s_quote= %s\n", all.lexer->s_quote ? "true" : "false");
 	ft_exit("pas de in put dans main go break, exit", &all, all.error_code); // ?? utile dans un monde parallele ?
-	(void)argv;
+	(void)argv; 
 }
 
