7341267fb07a9f79627ba8dabba13e7e3d575767
maxmaison <mdsiurds@gmail.com>
2025-07-09 23:12:02 +0200

    modif

--- built_in/export/print_and_assign.c
+++ built_in/export/print_and_assign.c
@@ -4,7 +4,8 @@
 void	print_and_null(t_all *all, char *s)
 {
 	ft_putstr_fd("WriteOnMe: export: `", 2);
-	ft_putstr_fd(s, 2);
+	if (s)
+		ft_putstr_fd(s, 2);
 	ft_putstr_fd("' not a valid identifier\n", 2);
 	all->data.n = NULL;
 	all->error_code = 1;
